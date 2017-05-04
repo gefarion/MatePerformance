5c70df565dce64b6a3cbb9f229d378251655d43b
Guido Chari <charig@gmail.com>
2017-05-04 15:52:52 +0200

    Remove echo. improve data-updating

--- Setup/update-data.sh
+++ Setup/update-data.sh
@@ -1,40 +1,45 @@
 #!/bin/bash
 set -e # make script fail on first error
 SCRIPT_PATH=`dirname $0`
-BUILDSCRIPTS=$SCRIPT_PATH/buildScripts
-source $BUILDSCRIPTS/basicFunctions.inc
+if [ ! "$1" = "guido@mostaza.cuartos.inv.dc.uba.ar" ]
+then
+    BUILDSCRIPTS=$SCRIPT_PATH/buildScripts
+    source $BUILDSCRIPTS/basicFunctions.inc
 
-export ROOT_PATH=$SCRIPT_PATH
+    export ROOT_PATH=$SCRIPT_PATH
 
-source $BUILDSCRIPTS/config.inc
+    source $BUILDSCRIPTS/config.inc
 
-MOSTAZA_ROOT_PATH='Documents/Writings/Research/optimizing-reflective-execution-environments/Experiments/Setup/'
-MOSTAZA_DATA_PATH=$MOSTAZA_ROOT_PATH/../Data
+    ROOT_PATH='Documents/Writings/Research/optimizing-reflective-execution-environments/Experiments/Setup/'
+    DATA_PATH=$ROOT_PATH/../Data
+fi
+
+
 
 date=`date "+%d-%m-%y"`
 name="experiments$date.tar.gz" 
 
 prepare_data() {
-    INFO Compressing data into name
-    tar -czvf $name *.data
-    OK done
-    INFO Moving data to $DATA_DIR
+    INFO Moving data from $ROOT_PATH to $DATA_DIR
     for file in *.data; do
         mv "$file" "$DATA_DIR/$file"
     done
-    mv $name $DATA_DIR/$name
+    pushd $DATA_DIR
+    INFO Compressing data into $name
+    tar -czvf $name *.data
+    popd > /dev/null
     OK done
 }
 
-if [ ! -z $1]
+if [ $# -eq 1 ]
 then
-    if [ "$1" = "zorzal"]
+    if [ "$1" = "zorzal" ]
     then
         ssh gchari@zorzal.dc.uba.ar 'bash -s' < $SCRIPT_PATH/$0 "guido@mostaza.cuartos.inv.dc.uba.ar"
         scp "gchari@zorzal.dc.uba.ar:$name" $DATA_DIR/
     else
-        ssh "guido@mostaza.cuartos.inv.dc.uba.ar" 'bash -s' < "$MOSTAZA_ROOT_PATH/update-data.sh"
-        scp "guido@mostaza.cuartos.inv.dc.uba.ar:$MOSTAZA_DATA_PATH/name" .
+        ssh "guido@mostaza.cuartos.inv.dc.uba.ar" "bash $ROOT_PATH/update-data.sh"
+        scp "guido@mostaza.cuartos.inv.dc.uba.ar:$DATA_PATH/name" .
     fi    
 else
     prepare_data
