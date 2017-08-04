aad72a35a555430a36a43a7146aa181071731348
Guido Chari <charig@gmail.com>
2017-08-04 15:02:53 -0300

    Put AWF benchs in particular directory to avoid git dir clashing with setup scripts

--- Setup/buildScripts/build-benchmarks.sh
+++ Setup/buildScripts/build-benchmarks.sh
@@ -9,7 +9,7 @@ if [ "$1" = "style" ]
 then
   exit 0
 else
-  checkout $ROOT_PATH "https://github.com/smarr/are-we-fast-yet.git" "master" "benchmarks"
+  checkout $ROOT_PATH/Benchmarks/AreWeFast "https://github.com/smarr/are-we-fast-yet.git" "master" "benchmarks"
   checkout "$BENCHMARKS_DIR/Mate" "https://github.com/charig/som.git" "reflectiveCompiler" "Examples/Benchmarks/*;Smalltalk/*"
   INFO Downloading MTG JSON data files 
   pushd $BENCHMARKS_DIR/Mate/Examples/Benchmarks/Mate/Tracing/ 
