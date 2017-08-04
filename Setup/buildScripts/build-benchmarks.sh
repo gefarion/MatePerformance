25a8cfc955f44cec6aa3de3316ebd35736d3df53
Guido Chari <charig@gmail.com>
2017-08-04 15:33:54 -0300

    Make the script create the benchmark dir in case it did not exist

--- Setup/buildScripts/build-benchmarks.sh
+++ Setup/buildScripts/build-benchmarks.sh
@@ -4,11 +4,15 @@ SCRIPT_PATH=`dirname $0`
 source $SCRIPT_PATH/basicFunctions.inc
 source $SCRIPT_PATH/config.inc
 
-INFO Build TruffleMATE
+INFO Build Benchmarks
 if [ "$1" = "style" ]
 then
   exit 0
 else
+  if [ ! -d $BENCHMARKS_DIR ]
+  then
+    mkdir $BENCHMARKS_DIR
+  fi
   checkout $ROOT_PATH/Benchmarks/AreWeFast "https://github.com/smarr/are-we-fast-yet.git" "master" "benchmarks"
   checkout "$BENCHMARKS_DIR/Mate" "https://github.com/charig/som.git" "reflectiveCompiler" "Examples/Benchmarks/*;Smalltalk/*"
   INFO Downloading MTG JSON data files 
