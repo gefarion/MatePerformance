d62e642be311922823ae60d11657d95d5ff9dba7
Guido Chari <charig@gmail.com>
2017-08-14 17:06:24 -0300

    add script for running experiments

---
+++ runExperiments.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+SCRIPT_PATH=`dirname $0`
+
+ALL=false
+
+if [ "$#" -eq 0 ]
+then
+    ALL=true
+fi
+
+if [[ "$ALL" = true || "$@" == "dynMetrics"  ]]
+then
+    ./runScripts/runDynMetrics.sh
+fi
