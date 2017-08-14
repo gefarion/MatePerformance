d560e326b1c00dd6f93febf91337d7ffae86020b
Guido Chari <charig@gmail.com>
2017-08-14 15:59:49 -0300

    RTruffle fix dirs

--- Setup/buildScripts/build-mate.sh
+++ Setup/buildScripts/build-mate.sh
@@ -15,21 +15,15 @@ else
   fi
   checkout $ROOT_PATH/Implementations/TruffleMate/Standard "https://github.com/charig/TruffleMATE.git" "reflectiveCompiler"
   pushd $ROOT_PATH/Implementations/TruffleMate/Standard
-  pushd libs/truffle
-  if [ ! -d "java-jvmci-8" ]
-  then
-    find_and_link JAVA_JVMCI_DIR "java-jvmci-8" "/home/guido/Documents/Projects/TruffleMATE/libs/truffle/java-jvmci-8/"
-  fi
-  popd > /dev/null
   INFO Compiling TruffleMATE
   make clean; make
   OK TruffleMATE Build Completed.
   popd > /dev/null
-  checkout $ROOT_PATH/Implementations/RTruffleMate/ "https://github.com/charig/RTruffleMATE.git" "dev"
+  checkout $ROOT_PATH/Implementations/RTruffleMate/ "https://github.com/charig/RTruffleMATE.git" "meta-level"
   pushd $ROOT_PATH/Implementations/RTruffleMate/
   if [ ! -d "pypy" ]
   then
-    find_and_link PYPY_DIR "pypy" "/home/guido/Documents/Projects/RTruffleMate/pypy/pypy /home/guido/Documents/Projects/RTruffleMate/pypy /home/guido/Library/Logs/Homebrew/pypy"
+    find_and_link PYPY_DIR "pypy" "/home/guido/Documents/Projects/pypy"
   fi
   INFO "Compiling RTruffleMATE in JIT mode (may take some time...)"
   make clean; JIT=True make
