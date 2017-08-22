162cfe41c842e584e614318362bea667ab42cc48
Guido Chari <charig@gmail.com>
2017-08-22 15:35:18 -0300

    Add RTruffleMate executable

---
+++ Setup/Implementations/rmate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+SCRIPT_PATH=`dirname $0`
+source $SCRIPT_PATH/../buildScripts/config.inc
+ARGS=1
+if [ "$1" == "--jit" ]
+then
+    BIN=RTruffleSOM-jit
+    ARGS=$((ARGS + 1))
+else
+    BIN=RTruffleSOM-no-jit
+fi
+if [ "$1" == "--envInObject" ]
+then
+    DIR=$SCRIPT_PATH/RTruffleMate/EnvironmentInObject
+    ARGS=$ARGS + 1
+else
+    DIR=$SCRIPT_PATH/RTruffleMate
+fi
+
+echo $DIR/$BIN ${@:$ARGS}
+exec $DIR/$BIN ${@:$ARGS}
