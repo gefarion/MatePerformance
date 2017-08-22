e432a31da60f79ef29a4ca04d98c8b024be8c9ae
Guido Chari <charig@gmail.com>
2017-08-22 15:41:43 -0300

    Some more work on RTruffleMate executable

--- Setup/Implementations/rmate.sh
+++ Setup/Implementations/rmate.sh
@@ -9,7 +9,7 @@ then
 else
     BIN=RTruffleSOM-no-jit
 fi
-if [ "$1" == "--envInObject" | "$2" == "--envInObject" ]
+if [ "$1" == "--envInObject" || "$2" == "--envInObject" ]
 then
     DIR=$SCRIPT_PATH/RTruffleMate/EnvironmentInObject
     ARGS=$ARGS + 1
