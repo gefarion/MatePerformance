fa02d12c43e76b06029d746cdd7f862f5375231b
Guido Chari <charig@gmail.com>
2017-09-12 14:14:33 -0300

    Fix images path when moving the experiments to the paper

--- updatePaperData.sh
+++ updatePaperData.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
-SCRIPT_PATH=`dirname $0`
+pushd `dirname $0` > /dev/null
+SCRIPT_PATH=`pwd`
+popd > /dev/null
 
 PAPER_PATH=$SCRIPT_PATH/../../Escritos/Research/optimizing-reflective-execution-environments/ 
+TESIS_PATH=$SCRIPT_PATH/../../Escritos/Research/TesisPhd/
+
 cp Report/images/*.pdf $PAPER_PATH/images/
+cp Report/images/*.pdf $TESIS_PATH/Imagenes/
 cp Report/experiments.tex $PAPER_PATH/sections/experiments.tex
+cp Report/experiments.tex $TESIS_PATH/Secciones/experiments.tex
+
+SUBS="\/"
+SCRIPT_PATH_ESCAPED=${SCRIPT_PATH////$SUBS}
+
+IMAGES_PATH="$SCRIPT_PATH_ESCAPED\/Report\/images"
+PAPER_IMAGES_PATH="\.\.\/images"
+TESIS_IMAGES_PATH="\.\.\/Imagenes"
+
+sed -i -e "s/$IMAGES_PATH/$PAPER_IMAGES_PATH/g" $PAPER_PATH/sections/experiments.tex
+sed -i -e "s/$IMAGES_PATH/$TESIS_IMAGES_PATH/g" $TESIS_PATH/Secciones/experiments.tex
