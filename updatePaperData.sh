6a8005a15c1a1ad8102e0266d70d4305a3aca2db
Guido Chari <charig@gmail.com>
2017-10-10 16:05:48 -0300

    Make updating of data compatible with my 2 settings

--- updatePaperData.sh
+++ updatePaperData.sh
@@ -3,8 +3,15 @@ pushd `dirname $0` > /dev/null
 SCRIPT_PATH=`pwd`
 popd > /dev/null
 
-PAPER_PATH=$SCRIPT_PATH/../../Escritos/Research/optimizing-reflective-execution-environments/ 
-TESIS_PATH=$SCRIPT_PATH/../../Escritos/Research/TesisPhd/
+if [ -d $SCRIPT_PATH/../../Writing ]
+then
+    TESIS_PATH=$SCRIPT_PATH/../../Writing/Research/TesisPhd
+    PAPER_PATH=$SCRIPT_PATH/../../Writing/Research/optimizing-reflective-execution-environments
+else
+    TESIS_PATH=$SCRIPT_PATH/../../Escritos/Research/TesisPhd
+    PAPER_PATH=$SCRIPT_PATH/../../Escritos/Research/optimizing-reflective-execution-environments/
+fi
+
 
 cp Report/images/*.pdf $PAPER_PATH/images/
 cp Report/images/*.pdf $TESIS_PATH/Imagenes/
@@ -16,7 +23,8 @@ SCRIPT_PATH_ESCAPED=${SCRIPT_PATH////$SUBS}
 
 IMAGES_PATH="$SCRIPT_PATH_ESCAPED\/Report\/images"
 PAPER_IMAGES_PATH="\.\.\/images"
-TESIS_IMAGES_PATH="\.\.\/Imagenes"
+#TESIS_IMAGES_PATH="\.\.\/Imagenes"
+TESIS_IMAGES_PATH="Imagenes"
 
 sed -i -e "s/$IMAGES_PATH/$PAPER_IMAGES_PATH/g" $PAPER_PATH/sections/experiments.tex
 sed -i -e "s/$IMAGES_PATH/$TESIS_IMAGES_PATH/g" $TESIS_PATH/Secciones/experiments.tex
