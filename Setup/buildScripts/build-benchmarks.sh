a8d2eacb1954902e724140c12bbb01bd8f98c262
Guido Chari <charig@gmail.com>
2017-11-30 14:25:03 -0300

    More cleaning

--- Setup/buildScripts/build-benchmarks.sh
+++ Setup/buildScripts/build-benchmarks.sh
@@ -15,13 +15,6 @@ else
   fi
   checkout $ROOT_PATH/Benchmarks/AreWeFast "https://github.com/charig/are-we-fast-yet.git" "master" "benchmarks"
   checkout "$BENCHMARKS_DIR/Mate" "https://github.com/charig/som.git" "reflectiveCompiler" "Examples/Benchmarks/*;Smalltalk/*"
-  INFO Downloading MTG JSON data files 
-  pushd $BENCHMARKS_DIR/Mate/Examples/Benchmarks/Mate/Tracing/ 
-  download_zip "AllSets-x.json.zip" "https://mtgjson.com/json/AllSets-x.json.zip"
-  download_zip "AllSets.json.zip" "https://mtgjson.com/json/AllSets.json.zip"
-  python cleanJsonFile "AllSets-x"
-  python cleanJsonFile "AllSets"
-  popd
   $SCRIPT_PATH/makeBenchmarks.sh
 fi
 OK TruffleMATE Build Completed.
