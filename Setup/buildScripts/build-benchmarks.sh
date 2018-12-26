48a9ae3af3fbdef2bba9ce87a9338b003ef3ed7b
Guido Chari <charig@gmail.com>
2018-12-26 16:43:46 +0100

    Remove comments

--- Setup/buildScripts/build-benchmarks.sh
+++ Setup/buildScripts/build-benchmarks.sh
@@ -14,15 +14,15 @@ if [ ! -d $BENCHMARKS_DIR ]
 then
   mkdir $BENCHMARKS_DIR
 fi
-#checkout "$BENCHMARKS_DIR/AreWeFast" "https://github.com/charig/are-we-fast-yet.git" "master" "benchmarks"
-#checkout "$BENCHMARKS_DIR/Mate" "https://github.com/charig/som.git" "reflectiveCompiler" "Examples/Benchmarks/*;Smalltalk/*"
-#INFO Downloading MTG JSON data files 
-#pushd $BENCHMARKS_DIR/Mate/Examples/Benchmarks/Mate/Tracing/ 
-#download_zip "AllSets-x.json.zip" "https://mtgjson.com/json/AllSets-x.json.zip"
-#download_zip "AllSets.json.zip" "https://mtgjson.com/json/AllSets.json.zip"
-#python cleanJsonFile "AllSets-x"
-#python cleanJsonFile "AllSets"
-#popd
+checkout "$BENCHMARKS_DIR/AreWeFast" "https://github.com/charig/are-we-fast-yet.git" "master" "benchmarks"
+checkout "$BENCHMARKS_DIR/Mate" "https://github.com/charig/som.git" "reflectiveCompiler" "Examples/Benchmarks/*;Smalltalk/*"
+INFO Downloading MTG JSON data files 
+pushd $BENCHMARKS_DIR/Mate/Examples/Benchmarks/Mate/Tracing/ 
+download_zip "AllSets-x.json.zip" "https://mtgjson.com/json/AllSets-x.json.zip"
+download_zip "AllSets.json.zip" "https://mtgjson.com/json/AllSets.json.zip"
+python cleanJsonFile "AllSets-x"
+python cleanJsonFile "AllSets"
+popd
 $SCRIPT_PATH/makeBenchmarks.sh
 
 OK TruffleMATE Build Completed.
