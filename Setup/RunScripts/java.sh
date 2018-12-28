8831abb751a3e4d0ffb9d5d1def9ec1668bce1d2
Guido Chari <charig@gmail.com>
2018-12-28 13:58:02 +0100

    Fix and rename java executable

---
+++ Setup/RunScripts/java.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SCRIPT_PATH="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"
+if [ ! -d $SCRIPT_PATH ]; then
+    echo "Could not determine absolute dir of $0"
+    echo "Maybe accessed with symlink"
+fi
+
+BUILDSCRIPTS="$SCRIPT_PATH/../BuildScripts"
+source "$BUILDSCRIPTS/basicFunctions.inc"
+
+exec java "$@"
