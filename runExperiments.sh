937bb7234af3fe54b4de2fa02d5a1f5f5c38b984
Guido Chari <charig@gmail.com>
2019-06-14 16:06:01 +0200

    More fixes

--- runExperiments.sh
+++ runExperiments.sh
@@ -25,7 +25,7 @@ then
     systemctl stop cron
     systemctl stop ondemand
     
-    docker run --privileged=true -v "$SCRIPT_PATH/Data:/Data" test_mate \
+echo    docker run --privileged=true -v "$SCRIPT_PATH/Data:/Data" test_mate \
         /opt/MatePerformance/Scripts/runBenchs.sh /opt/MatePerformance/mate.conf /opt/Benchmarks/AreWeFast/ /opt/Som/ /opt/TruffleMate/ /opt/RTruffleMate/MOInShape/ /opt/RTruffleMate/MOInObject /opt/Pharo /opt/openjdk8JVMCI /opt/graal \
         $@
     
