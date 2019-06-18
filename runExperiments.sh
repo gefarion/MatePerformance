d3c5c67f2a995fa55aa11c80698f54d8d3533caa
Guido Chari <charig@gmail.com>
2019-06-18 11:54:53 +0200

    Rebench conf increase size

--- runExperiments.sh
+++ runExperiments.sh
@@ -21,9 +21,9 @@ fi
 if [[ "$ALL" == true || "$1" == "AreWeFast" || "$1" == "Inherent" || "$1" == "IndividualActivations" || "$1" == "Readonly" || "$1" == "Tracing" || "$1" == "ReflectiveCompilation" ]]
 then
     set -e # make script fail on first error
-    systemctl stop gdm
-    systemctl stop cron
-    systemctl stop ondemand
+#    systemctl stop gdm
+#    systemctl stop cron
+#    systemctl stop ondemand
     
 echo    docker run --privileged=true -v "$SCRIPT_PATH/Data:/Data" test_mate \
         /opt/MatePerformance/Scripts/runBenchs.sh /opt/MatePerformance/mate.conf /opt/Benchmarks/AreWeFast/ /opt/Som/ /opt/TruffleMate/ /opt/RTruffleMate/MOInShape/ /opt/RTruffleMate/MOInObject /opt/Pharo /opt/openjdk8JVMCI /opt/graal \
